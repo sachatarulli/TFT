4e9e91ca8c7c7453a833b1099a6e82dcdccf60c2
RodriCalvo <rodrigo_javiercalvo@hotmail.com>
2025-02-15 22:30:19 -0300

    proyecto casi terminado

--- TFT/src/PRUEBAS.java
+++ TFT/src/PRUEBAS.java
@@ -3,9 +3,34 @@ import java.util.Scanner;
 public class PRUEBAS {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         Juego juego = new Juego(true);
         juego.jugar();
+        juego.votacion();
     }
+
+    /*public void menu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        System.out.println("-----------------------------------");
+        System.out.println("Fútbol Ruleta: ¡Armá tu Dream Team!");
+        System.out.println("-----------------------------------");
+        System.out.println("1. Jugar");
+        System.out.println("2. Información del proyecto");
+        System.out.println("3. Salir");
+        System.out.println("-----------------------------------");
+        System.out.println("Seleccione una opcion: ");
+        opcion = scanner.nextInt();
+
+        do {
+            menu();
+            switch (opcion) {
+                case 1:
+            }
+
+            if ()
+
+        } while ()
+    }*/
+
 }
